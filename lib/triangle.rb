b806a1c806dcc0c1aa2530e6db480460813a7143
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-02 21:49:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   def kind
     if @side1 == @side2 && @side2 == @side3
       :equilateral
-    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3 
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
       :isosceles
     else
       :scalene
@@ -20,7 +20,7 @@ class Triangle
   end
 
   def valid_triangle
-    
+
 
   class TriangleError < StandardError
   end
